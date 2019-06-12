dde977ad37f592539fe3ffd472c13a52ce9ddd2a
zhl1992525@163.com <zhl1992525@163.com>
2019-06-12 15:32:30 +0800

    update

--- src/internal/khsy.js
+++ src/internal/khsy.js
@@ -7,7 +7,7 @@ const khsy = {
   $request_url: 'http://139.159.148.80:8091', // 请求接口   
   $version: '0.0.1',
   $app_id: 1, // 未来可能会用于区别客户端；例如：1代表小程序入口
-  $token_key: 'KHSY_TOKEN_KEY'
+  $token_key: 'TOKEN_KEY'
 }
 
 export default khsy
